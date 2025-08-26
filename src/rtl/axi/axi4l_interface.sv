6d3407c9a016ce1b45dcb84b21e345b367e9e5eb
Davidmachadocb <davidmachadocbec@gmail.com>
2025-08-26 11:12:23 -0300

    fix: wrong signals size and missing signals in interface

--- src/rtl/axi/axi4l_interface.sv
+++ src/rtl/axi/axi4l_interface.sv
@@ -15,7 +15,7 @@ interface axi4_lite #(
 
     //Read Data Channel - R
     logic [DATA_WIDTH:0]    RDATA;
-    logic                   RRESP;
+    logic [1:0]             RRESP;
     logic                   RVALID;
     logic                   RREADY;
 
@@ -29,30 +29,30 @@ interface axi4_lite #(
     //Write Data Channel - W
     logic [DATA_WIDTH]      WDATA;
     logic [3:0]             WSTRB;
+    logic                   WVALID;
+    logic                   WREADY;
 
     //Write Response Channel - B
-    logic                   BRESP;
+    logic [1:0]             BRESP;
     logic                   BVALID;
     logic                   BREADY;
 
     modport master_ports (
         input ACLK, ARESETN,
-        input ARREADY, RDATA, RRESP,
-        input RVALID, AWREADY, BRESP,
-        input BVALID,
-        output ARADDR, ARCACHE, ARPROT, ARVALID,
-        output RREADY, AWADDR, AWCACHE, AWPROT,
-        output AWVALID, WDATA, WSTRB, BREADY
+        input ARREADY, RDATA, RRESP, RVALID,
+        input AWREADY, WREADY, BRESP, BVALID,
+        output ARADDR, ARCACHE, ARPROT, ARVALID, RREADY,
+        output AWADDR, AWCACHE, AWPROT, AWVALID,
+        output WDATA, WSTRB, WVALID, BREADY
     );
 
     modport slave_ports (
         input ACLK, ARESETN,
-        input ARADDR, ARCACHE, ARPROT, ARVALID,
-        input RREADY, AWADDR, AWCACHE, AWPROT,
-        input AWVALID, WDATA, WSTRB, BREADY
-        output ARREADY, RDATA, RRESP,
-        output RVALID, AWREADY, BRESP,
-        output BVALID
+        input ARADDR, ARCACHE, ARPROT, ARVALID, RREADY,
+        input AWADDR, AWCACHE, AWPROT, AWVALID,
+        input WDATA, WSTRB, WVALID, BREADY,
+        output ARREADY, RDATA, RRESP, RVALID,
+        output AWREADY, WREADY, BRESP, BVALID
     );
 
 
